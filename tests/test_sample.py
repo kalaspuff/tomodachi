cdfa7058f610b5a6632f1b4ca9c78e56836bd757
Carl Oscar Aaro <hello@carloscar.com>
2017-03-25 01:20:37 +0100

    Codecov tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,5 @@
 import tomodachi
-import tomodachi.cli
 
 
-def test_version():
-    tomodachi.cli.CLI()
-    assert tomodachi.__version__ == '0.1.5'
+def test_version_exists():
+    assert tomodachi.__version__ is not None
