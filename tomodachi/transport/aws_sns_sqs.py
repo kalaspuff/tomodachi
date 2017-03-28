2cf5eb16f61b586362c2776ea4a8303b24202397
Carl Oscar Aaro <hello@carloscar.com>
2017-03-28 09:40:38 +0200

    Use correct error for new aiohttp 2.0

--- tomodachi/transport/aws_sns_sqs.py
+++ tomodachi/transport/aws_sns_sqs.py
@@ -266,7 +266,7 @@ class AWSSNSSQSTransport(Invoker):
             error_message = str(e)
             logging.getLogger('transport.aws_sns_sqs').warn('Unable to connect [sqs] to AWS ({})'.format(error_message))
             raise AWSSNSSQSConnectionException(str(e), log_level=context.get('log_level')) from e
-        except aiohttp.errors.ClientOSError as e:
+        except aiohttp.client_exceptions.ClientOSError as e:
             error_message = str(e)
             logging.getLogger('transport.aws_sns_sqs').warn('Unable to connect [sqs] to AWS ({})'.format(error_message))
             raise AWSSNSSQSConnectionException(str(e), log_level=context.get('log_level')) from e
