b9c6a7afe1694a4e3d8058d65a31867b5dfcc646
Carl Oscar Aaro <hello@carloscar.com>
2018-02-22 11:45:59 +0100

    Tests for relative imports

---
+++ tests/services/relative_service.py
@@ -0,0 +1,21 @@
+import tomodachi
+from .relative_import.import_file import noop
+
+
+@tomodachi.service
+class RelativeService(object):
+    name = 'test_relative'
+
+    start = False
+    started = False
+    stop = False
+
+    async def _start_service(self) -> None:
+        noop()
+        self.start = True
+
+    async def _started_service(self) -> None:
+        self.started = True
+
+    async def _stop_service(self) -> None:
+        self.stop = True
