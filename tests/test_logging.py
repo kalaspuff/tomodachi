4125632db185523ae128a49b234137dcf14f91a6
Carl Oscar Aaro <hello@carloscar.com>
2018-02-26 19:45:38 +0100

    Added easier logging capabilities to services via self.log() and self.log_setup()

---
+++ tests/test_logging.py
@@ -0,0 +1,25 @@
+import os
+from typing import Any
+from run_test_service_helper import start_service
+
+
+def test_logging_service(monkeypatch: Any, capsys: Any, loop: Any) -> None:
+    log_path = '/tmp/7815c7d6-5637-4bfd-ad76-324f4329a6b8.log'
+
+    services, future = start_service('tests/services/logging_service.py', monkeypatch)
+
+    assert services is not None
+    instance = services.get('test_logging')
+    assert instance is not None
+
+    loop.run_until_complete(future)
+
+    log_content = str(open(log_path).read().strip())
+    assert '_start_service' in log_content
+    assert '_started_service' in log_content
+    assert '_stop_service' in log_content
+
+    try:
+        os.remove(log_path)
+    except OSError:
+        pass
