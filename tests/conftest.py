ad1acd7f369893eb6d4abf963d83c835fa0c1b48
Carl Oscar Aaro <hello@carloscar.com>
2018-02-26 12:14:09 +0100

    Fixes for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,7 @@ def loop() -> Generator:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     yield loop
+    try:
+        loop.close()
+    except RuntimeWarning:
+        pass
