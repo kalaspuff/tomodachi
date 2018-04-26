43905bccf5b90b00e53a46173ac7dff2b69718e4
Carl Oscar Aaro <hello@carloscar.com>
2018-04-26 10:54:26 +0200

    Corrects typehinting

--- tomodachi/protocol/__init__.py
+++ tomodachi/protocol/__init__.py
@@ -1,9 +1,9 @@
 from typing import Any
 from tomodachi.protocol.json_base import JsonBase
 try:
-    from tomodachi.protocol.protobuf_base import ProtobufBase
-except ModuleNotFoundError:
-    class ProtobufBase(object):
+    from tomodachi.protocol.protobuf_base import ProtobufBase  # type: ignore
+except Exception:
+    class ProtobufBase(object):  # type: ignore
         @classmethod
         def validate(cls, **kwargs: Any) -> None:
             raise Exception('google.protobuf package not installed')
