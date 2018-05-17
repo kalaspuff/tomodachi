b741236f8bfdedccc27231e090ea8743163aee25
Carl Oscar Aaro <hello@carloscar.com>
2018-05-17 23:52:25 +0200

    Removed legacy code

--- tomodachi/invoker/decorator.py
+++ tomodachi/invoker/decorator.py
@@ -58,37 +58,3 @@ def decorator(include_function: bool=False):
     if fn:
         return _decorator(fn)
     return _decorator
-
-
-#@deco.decorator
-#def ___decorator(func, *args, **kargs):
-#    return func(*args, **kwargs)
-#
-#
-#def __decorator(include_function: bool=False) -> Callable:
-#    fn = None
-#    if include_function and callable(include_function):
-#        fn = include_function
-#        include_function = False
-#
-#    def _decorator(decorator_func: Callable) -> Callable:
-#        def _wrapper(func: Callable) -> Callable:
-#            @wraps(func)
-#            async def wrapper(*a: Any, **kw: Any) -> Any:
-#                if not include_function:
-#                    return_value = decorator_func(*a, **kw)
-#                else:
-#                    return_value = decorator_func(func, *a, **kw)
-#
-#                return_value = (await return_value) if isinstance(return_value, Awaitable) else return_value
-#                if return_value is True or return_value is None:
-#                    routine = func(*a, **kw)
-#                    return (await routine) if isinstance(routine, Awaitable) else routine
-#                return return_value
-#
-#            return wrapper
-#        return _wrapper
-#
-#    if fn:
-#        return _decorator(fn)
-#    return _decorator
