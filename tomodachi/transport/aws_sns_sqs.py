a1e2efc1abe6f4e2de4a580e58184323660b4299
Kimmo Parviainen-Jalanko <k@77.fi>
2019-05-11 01:37:59 +0300

    FFS, you can just omit all the credentials and it's handled by AWS automatically.
    2 working days well wasted.

--- tomodachi/transport/aws_sns_sqs.py
+++ tomodachi/transport/aws_sns_sqs.py
@@ -238,31 +238,10 @@ class AWSSNSSQSTransport(Invoker):
         loop = asyncio.get_event_loop()
         session = aiobotocore.get_session(loop=loop)
 
-        config_base = context.get('options', {}).get('aws_sns_sqs', context.get('options', {}).get('aws', {}))
-        aws_config_base = context.get('options', {}).get('aws', {})
-
-        region_name = config_base.get('aws_region_name', config_base.get('region_name')) or \
-            aws_config_base.get('aws_region_name', aws_config_base.get('region_name'))
-        aws_secret_access_key = config_base.get('aws_secret_access_key', config_base.get('secret_access_key')) or \
-            aws_config_base.get('aws_secret_access_key', aws_config_base.get('secret_access_key'))
-        aws_access_key_id = config_base.get('aws_access_key_id', config_base.get('access_key_id')) or \
-            aws_config_base.get('aws_access_key_id', aws_config_base.get('access_key_id'))
-        endpoint_url = config_base.get('aws_endpoint_urls', config_base.get('endpoint_urls', {})).get(name) or \
-            config_base.get('aws_{}_endpoint_url'.format(name), config_base.get('{}_endpoint_url'.format(name))) or \
-            aws_config_base.get('aws_endpoint_urls', aws_config_base.get('endpoint_urls', {})).get(name) or \
-            config_base.get('aws_endpoint_url', config_base.get('endpoint_url')) or \
-            aws_config_base.get('aws_endpoint_url', aws_config_base.get('endpoint_url')) or \
-            context.get('options', {}).get('aws_endpoint_urls', {}).get(name)
-        try:
-            token = context['options']['aws_session_token']
-            aws_session_token = token()
-        except (AttributeError, KeyError):
-            aws_session_token = None
-
         try:
             if cls.clients_creation_time.get(name) and cls.clients_creation_time[name] + 30 > time.time():
                 return
-            cls.clients[name] = session.create_client(name, region_name=region_name, aws_secret_access_key=aws_secret_access_key, aws_access_key_id=aws_access_key_id, aws_session_token=aws_session_token, endpoint_url=endpoint_url)
+            cls.clients[name] = session.create_client(name, region_name='eu-west-1')
             cls.clients_creation_time[name] = time.time()
         except (botocore.exceptions.PartialCredentialsError, botocore.exceptions.NoRegionError) as e:
             error_message = str(e)
