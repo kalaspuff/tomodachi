94f2ee827c0adbed00d561cd0a590d11454a1926
Carl Oscar Aaro <hello@carloscar.com>
2020-10-13 06:46:48 +0200

    Added test for receiving messages on message_attributes and filter_policy

--- tests/services/aws_sns_sqs_service_with_credentials.py
+++ tests/services/aws_sns_sqs_service_with_credentials.py
@@ -1,8 +1,9 @@
 import asyncio
+import json
 import os
 import signal
 import uuid
-from typing import Any
+from typing import Any, Dict, Set
 
 import tomodachi
 from tomodachi.discovery.aws_sns_registration import AWSSNSRegistration
@@ -35,14 +36,17 @@ class AWSSNSSQSService(tomodachi.Service):
     test_topic_specified_queue_name_data_received = False
     test_topic_metadata_topic = None
     test_topic_service_uuid = None
+    test_message_attribute_currencies: Set = set()
+    test_message_attribute_amounts: Set = set()
     wildcard_topic_data_received = False
     data_uuid = data_uuid
 
-    def check_closer(self):
+    def check_closer(self) -> None:
         if (
             self.test_topic_data_received
             and self.test_topic_specified_queue_name_data_received
             and self.wildcard_topic_data_received
+            and len(self.test_message_attribute_currencies) == 7
         ):
             if not self.closer.done():
                 self.closer.set_result(None)
@@ -53,6 +57,32 @@ class AWSSNSSQSService(tomodachi.Service):
             self.test_topic_data_received = True
             self.test_topic_metadata_topic = metadata.get("topic")
             self.test_topic_service_uuid = service.get("uuid")
+
+            self.check_closer()
+
+    @aws_sns_sqs(
+        "test-topic-filtered",
+        queue_name="test-queue-filter-currency-{}".format(data_uuid),
+        filter_policy={"currency": ["SEK", "EUR", "USD", "GBP", "CNY"]},
+    )
+    async def test_filter_policy_currency(
+        self, data: Any, queue_url: str, receipt_handle: str, message_attributes: Dict
+    ) -> None:
+        if data == self.data_uuid and queue_url and receipt_handle:
+            self.test_message_attribute_currencies.add(json.dumps(message_attributes, sort_keys=True))
+
+            self.check_closer()
+
+    @aws_sns_sqs(
+        "test-topic-filtered",
+        queue_name="test-queue-filter-amount-{}".format(data_uuid),
+        filter_policy={"currency": ["SEK", "EUR", "USD", "GBP", "CNY"], "amount": [{"numeric": [">=", 99.51]}]},
+    )
+    async def test_filter_policy_amount(
+        self, data: Any, queue_url: str, receipt_handle: str, message_attributes: Dict
+    ) -> None:
+        if data == self.data_uuid and queue_url and receipt_handle:
+            self.test_message_attribute_amounts.add(json.dumps(message_attributes, sort_keys=True))
 
             self.check_closer()
 
@@ -82,12 +112,12 @@ class AWSSNSSQSService(tomodachi.Service):
             self.check_closer()
 
     async def _started_service(self) -> None:
-        async def publish(data: Any, topic: str) -> None:
-            await aws_sns_sqs_publish(self, data, topic=topic, wait=False)
+        async def publish(data: Any, topic: str, **kwargs: Any) -> None:
+            await aws_sns_sqs_publish(self, data, topic=topic, wait=False, **kwargs)
 
         async def _async() -> None:
             async def sleep_and_kill() -> None:
-                await asyncio.sleep(10.0)
+                await asyncio.sleep(30.0)
                 if not self.closer.done():
                     self.closer.set_result(None)
 
@@ -100,7 +130,34 @@ class AWSSNSSQSService(tomodachi.Service):
         asyncio.ensure_future(_async())
 
         self.data_uuid = str(uuid.uuid4())
+
+        await publish(self.data_uuid, "test-topic")
         await publish(self.data_uuid, "test-topic-unique")
+
+        await publish(self.data_uuid, "test-topic-filtered")
+        for ma in (
+            {},
+            {"currency": "DKK"},
+            {"currency": "sek"},
+            {"currency": ["SEK"], "amount": 1338, "value": "1338.00 SEK"},
+            {"value": "100 SEK"},
+            {"currency": "100 SEK"},
+            {"currency": "NOK"},
+            {"currency": "USD", "amount": 99.50, "value": "99.50 USD"},
+            {"amount": 4000},
+            {"currency": "EUR"},
+            {"currency": ["BTC", "ETH"]},
+            {"currency": ["JPY"]},
+            {"currency": ["GBP"]},
+            {"currency": "EUR"},
+            {"currency": "SEK", "amount": 4711},
+            {"currency": "SEK", "amount": 4711},
+            {"currency": "SEK", "amount": "9001.00"},
+            {"currency": ["CNY", "USD", "JPY"]},
+            {"currency": "EUR"},
+        ):
+            await publish(self.data_uuid, "test-topic-filtered", message_attributes=ma)
+
         for _ in range(30):
             if self.test_topic_data_received:
                 break
