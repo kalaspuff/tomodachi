fa95164bea4104121225bf0e296f666a16af7e40
Carl Oscar Aaro <hello@carloscar.com>
2020-10-22 14:20:36 +0200

    # Work-around if the decorators are stacked with multiple decorators for the same method

--- tomodachi/invoker/base.py
+++ tomodachi/invoker/base.py
@@ -1,6 +1,6 @@
 import functools
 import types
-from typing import Any, Callable, Dict, List, Optional  # noqa
+from typing import Any, Callable, Dict, List, Optional, Tuple, cast  # noqa
 
 FUNCTION_ATTRIBUTE = "_tomodachi_function_is_invoker_function"
 START_ATTRIBUTE = "_tomodachi_deprecated_invoker_function_start_marker"
@@ -37,7 +37,26 @@ class Invoker(object):
                     context = cls.context[obj]
                     obj.context = context
                     start_func = await cls_func(cls, obj, context, func, *args, **kwargs)
+
+                    if getattr(func, FUNCTION_ATTRIBUTE, None):
+                        decorated_cls_func = cast(Callable, getattr(func, "cls_func"))
+                        decorated_args = cast(Tuple, getattr(func, "args"))
+                        decorated_kwargs = cast(Dict, getattr(func, "kwargs"))
+                        decorated_func = cast(Callable, getattr(func, "func"))
+                        fn = cast(
+                            Callable,
+                            cls.decorator(decorated_cls_func)(*decorated_args, **decorated_kwargs)(decorated_func),
+                        )
+                        setattr(fn, START_ATTRIBUTE, True)
+                        await fn(obj, *args, **kwargs)
+
                     return start_func
+
+                # Work-around if the decorators are stacked with multiple decorators for the same method
+                setattr(_decorator, "func", func)
+                setattr(_decorator, "cls_func", cls_func)
+                setattr(_decorator, "args", args)
+                setattr(_decorator, "kwargs", kwargs)
 
                 setattr(_decorator, FUNCTION_ATTRIBUTE, True)
                 return _decorator
