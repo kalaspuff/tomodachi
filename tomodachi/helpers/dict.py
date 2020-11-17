047f7bfa6d15e1c36a69edd5c92b6bece343e2f1
Carl Oscar Aaro <hello@carloscar.com>
2020-11-17 17:57:14 +0100

    Minor optimization for merge_dict

--- tomodachi/helpers/dict.py
+++ tomodachi/helpers/dict.py
@@ -2,6 +2,13 @@ from typing import Dict
 
 
 def merge_dicts(dict1: Dict, dict2: Dict) -> Dict:
+    if dict1 and not dict2:
+        return dict(dict1)
+    elif dict2 and not dict1:
+        return dict(dict2)
+    elif not dict1 and not dict2:
+        return {}
+
     context = dict(dict1)
     for k, v2 in dict2.items():
         v1 = context.get(k)
