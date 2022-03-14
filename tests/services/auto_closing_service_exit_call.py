1a1527d10d9d3f14d18d23dcc3a69b835a2a9f1d
Carl Oscar Aaro <hello@carloscar.com>
2022-03-14 15:52:20 +0100

    Updated implementation tests to include testing termination with 'tomodachi.exit()'

--- tests/services/auto_closing_service_exit_call.py
+++ tests/services/auto_closing_service_exit_call.py
@@ -7,6 +7,7 @@ from tomodachi.discovery.dummy_registry import DummyRegistry
 from tomodachi.envelope.json_base import JsonBase
 
 
+@tomodachi.service
 class AutoClosingService(tomodachi.Service):
     name = "test_auto_closing"
     discovery = [DummyRegistry]
@@ -17,15 +18,12 @@ class AutoClosingService(tomodachi.Service):
     stop = False
 
     async def _start_service(self) -> None:
-        self.log("_start_service() function called")
         self.start = True
 
     async def _started_service(self) -> None:
-        self.log("_started_service() function called")
         self.started = True
         await asyncio.sleep(0.1)
         tomodachi.exit()
 
     async def _stop_service(self) -> None:
-        self.log("_stop_service() function called")
         self.stop = True
