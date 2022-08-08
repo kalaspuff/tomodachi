e8951ea04c711a358aca0ef22d242f92ba2909b5
Carl Oscar Aaro <hello@carloscar.com>
2022-08-08 18:38:05 +0200

    Updated proto builds to support newer versions of protobuf

---
+++ tests/proto_build/message_pb2.pyi
@@ -0,0 +1,13 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Person(_message.Message):
+    __slots__ = ["id", "name"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    name: str
+    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
