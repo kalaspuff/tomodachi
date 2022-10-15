de7e55d3658c241af1e08f27f46ffa83a8d5dc8e
Carl Oscar Aaro <hello@carloscar.com>
2022-10-16 00:35:45 +0200

    Fix for AttributeError values only usable in Python 3.10

--- tomodachi/options/interface.py
+++ tomodachi/options/interface.py
@@ -34,8 +34,9 @@ class OptionsInterface:
     def __setattr__(self, item: str, value: Any) -> None:
         if not hasattr(self, item) and item not in self.keys():
             exc = AttributeError(f"'{type(self).__name__}' object has no attribute '{item}'")
-            exc.name = item
-            exc.obj = self
+            if hasattr(exc, "name") and hasattr(exc, "obj"):
+                exc.name = item
+                exc.obj = self
             raise exc
 
         super().__setattr__(item, value)
@@ -48,8 +49,9 @@ class OptionsInterface:
 
         if not hasattr(self, item):
             exc = AttributeError(f"'{type(self).__name__}' object has no attribute '{item}'")
-            exc.name = item
-            exc.obj = self
+            if hasattr(exc, "name") and hasattr(exc, "obj"):
+                exc.name = item
+                exc.obj = self
             raise exc
 
         setattr(self, item, value)
