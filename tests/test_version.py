41bf26ff90e9023e51e1d8f278ffaaf43c6df365
Carl Oscar Aaro <hello@carloscar.com>
2023-07-21 03:06:13 +0200

    fixes test_version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,4 +3,5 @@ import tomodachi
 
 def test_version_exists() -> None:
     assert tomodachi.__version__ is not None
-    assert isinstance(tomodachi.__version__, tuple)
+    assert isinstance(tomodachi.__version__, str)
+    assert isinstance(tomodachi.__version_info__, tuple)
