2afd85899e4350ce70c7c4027cf322b1ae1d578c
Carl Oscar Aaro <hello@carloscar.com>
2023-08-14 03:37:39 +0200

    Type hint annotation fix

--- tomodachi/envelope/__init__.pyi
+++ tomodachi/envelope/__init__.pyi
@@ -1,2 +1,4 @@
+from tomodachi.envelope import json_base as json_base
+from tomodachi.envelope import protobuf_base as protobuf_base
 from tomodachi.envelope.json_base import JsonBase as JsonBase
 from tomodachi.envelope.protobuf_base import ProtobufBase as ProtobufBase
