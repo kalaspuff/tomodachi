b12452c37a983c1c22e104bfb350551d412a76cf
Carl Oscar Aaro <hello@carloscar.com>
2024-07-05 11:22:15 +0200

    0.27.2

--- tomodachi/__version__.py
+++ tomodachi/__version__.py
@@ -4,7 +4,7 @@ __version_info__: Tuple[Union[int, str], ...] = (0, 27, 2)
 __version__: str = "".join([".{}".format(str(n)) if type(n) is int else str(n) for n in __version_info__]).replace(
     ".", "", 1 if type(__version_info__[0]) is int else 0
 )
-__build_time__ = ""
+__build_time__ = "2024-07-05T09:21:32.176232Z"
 
 if __name__ == "__main__":
     print(__version__)
