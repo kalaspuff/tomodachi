39872bea9b0f3f1c3608b389e10ed8d48329dc40
Carl Oscar Aaro <hello@carloscar.com>
2025-03-11 16:21:54 +0100

    post-release commit (sets version to 0.28.4dev0)

--- tomodachi/__version__.py
+++ tomodachi/__version__.py
@@ -1,10 +1,10 @@
 from typing import Tuple, Union
 
-__version_info__: Tuple[Union[int, str], ...] = (0, 28, 3)
+__version_info__: Tuple[Union[int, str], ...] = (0, 28, 4, "dev0")
 __version__: str = "".join([".{}".format(str(n)) if type(n) is int else str(n) for n in __version_info__]).replace(
     ".", "", 1 if type(__version_info__[0]) is int else 0
 )
-__build_time__ = "2025-03-11T15:21:20.984531Z"
+__build_time__ = ""
 
 if __name__ == "__main__":
     print(__version__)
